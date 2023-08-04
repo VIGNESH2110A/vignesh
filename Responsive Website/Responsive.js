f8166af1b4d20077a2de1ecafe01470057ddd9a7
VIGNESH2110A <114280422+VIGNESH2110A@users.noreply.github.com>
2023-08-04 11:10:47 +0530

    Add files via upload

---
+++ Responsive Website/Responsive.js
@@ -0,0 +1,14 @@
+let menu=()=>
+{
+    if(document.getElementById("menus").style.display=="none")
+    {
+        document.getElementById("menus").style.display="flex";
+
+
+    }
+    else
+    {
+        document.getElementById("menus").style.display="none";
+
+    }
+}
